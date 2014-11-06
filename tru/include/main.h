b08b5c91d20f388802ae8241047ba23d2e55120d
krebyy <kleber.ufu@hotmail.com>
2014-11-06 21:43:23 -0200

    Commit define da rampa e kp pelo trimpot.

--- tru/include/main.h
+++ tru/include/main.h
@@ -21,28 +21,27 @@
 using namespace std;
 using namespace cv;
 
+//#define DEBUG				// Retire o comentário para visualizar as mensagens de Debug
+//#define TRATAMENTO_RAMPA	// Retire o comentário para habilitar o tratamento da rampa (turbo e freio)
+
 #define N_COLUNAS   320     // Largura da imagem
 #define N_LINHAS    176     // Altura da imagem
 #define SP_X (N_COLUNAS / 2)    // Setpoint da direção em relação à imagem (centro da foto)
 
-// Valores auxiliares para o tratamento da rampa
-#define SUBIDA 1
-#define DESCIDA 2
-#define TEMPO_RAMPA 13		// valor * 100 ms: tempo do turbo ou freio
-#define INTERVALO_RAMPA 10	// valor * 100 ms: intervalo entre leituras dos eventos de rampa
-#define GY_SUBIDA -7000		// limiar do valor lido pelo giroscópio para identificar uma subida
-#define GY_DESCIDA 7000		// limiar do valor lido pelo giroscópio para identificar uma descida
+#ifdef TRATAMENTO_RAMPA
+	// Estruturas auxiliares para o tratamento da rampa
+	#define SUBIDA 1
+	#define DESCIDA 2
+	#define TEMPO_RAMPA 13		// valor * 100 ms: tempo do turbo ou freio
+	#define INTERVALO_RAMPA 10	// valor * 100 ms: intervalo entre leituras dos eventos de rampa
+	#define GY_SUBIDA -7000		// limiar do valor lido pelo giroscópio para identificar uma subida
+	#define GY_DESCIDA 7000		// limiar do valor lido pelo giroscópio para identificar uma descida
 
+	void* tickTimerThread(void *ticks_ptr);
 
-//#define DEBUG
-
-
-
-void* tickTimerThread(void *ticks_ptr);
-
-
-int rampa = 0;
-unsigned int ticks_rampa = 0;
+	int rampa = 0;
+	unsigned int ticks_rampa = 0;
+#endif
 
 
 #endif // MAIN_H
--- tru/src/main.cpp
+++ tru/src/main.cpp
@@ -5,10 +5,8 @@ int main()
 {
 	// Variáveis do programa principal
     int erro[2] = {0, 0}, n_linhas_ok = 0, servo = 0, esc = 0, x = 0, y = 0;
-    int v_min = 0, v_max = 0;
-    unsigned int ticks = 0;
-    int tempo_rampa = 0;
-    ticks_rampa = -10;
+    int v_min = 1500000, v_max = 1800000, kp = 40000;
+
 
     // Seleciona a frequência da BBB para 1GHz
     system("cpufreq-set -g performance");
@@ -17,16 +15,23 @@ int main()
     Mat src;
     Capturador imagem(N_COLUNAS, N_LINHAS);
     Reconhecedor reconhecimento(N_COLUNAS, N_LINHAS, 8, 30, 50, 1);
-    Controlador controle(N_COLUNAS, N_LINHAS, 0, 51200, 0, 1500000, 1800000);
+    Controlador controle(N_COLUNAS, N_LINHAS, kp, 51200, 0, v_min, v_max);
     Pwm pwms;
     Leds leds;
     Buttons buttons;
+    Trimpot trimpot0(0, 0, 51200);
     Trimpot trimpot1(1, 1500000, 1800000);
     Trimpot trimpot2(2, 1500000, 1800000);
 
-    // Declaração e criação da thread auxiliar
-    pthread_t tick_timer;
-    pthread_create(&tick_timer, NULL, tickTimerThread, &ticks);
+
+	#ifdef TRATAMENTO_RAMPA
+		// Declaração das variáveis necessárias para o tratamento da rampa e criação da thread auxiliar
+		unsigned int ticks = 0;
+		int tempo_rampa = 0;
+		ticks_rampa = -INTERVALO_RAMPA;
+		pthread_t tick_timer;
+		pthread_create(&tick_timer, NULL, tickTimerThread, &ticks);
+	#endif
 
 
 	// Inicialização do robô -------------------------------------------------------------------------------------------
@@ -40,17 +45,24 @@ int main()
     leds.setColor(RED);
     while(buttons.getStatus() != LEFT) usleep(250000);
 	#ifdef DEBUG
-		printf("3 segundos para o robo partir...\n");
+		printf("2 segundos para o robo partir...\n");
 	#endif
     pwms.initServo();
     pwms.initESC();
     v_min = trimpot2.getValue();
-    usleep(3000000);
-    v_max = trimpot1.getValue();
+	controle.setVelocMin(v_min);
+	usleep(50000);
+	v_max = trimpot1.getValue();
+	controle.setVelocMax(v_max);
+	usleep(50000);
+	kp = trimpot0.getValue();
+	controle.setKp(kp);
+	usleep(2000000);
     leds.setColor(GREEN);
     imagem.open();
-    ticks = 0;
-
+	#ifdef TRATAMENTO_RAMPA
+    	ticks = 0;
+	#endif
 	#ifdef DEBUG
 		printf("Robo em funcionamento...\n");
 
@@ -85,28 +97,28 @@ int main()
 
 		// Controle ----------------------------------------------------------------------------------------------------
 		servo = controle.direcao(erro);         // Executa o controlador de direção
-		controle.setVelocMin(v_min);
-		controle.setVelocMax(v_max);
 		esc = controle.velocidade(n_linhas_ok); // Executa o controlador de velocidade
 
 
-		// Tratamento da rampa -----------------------------------------------------------------------------------------
-		if (rampa == SUBIDA)
-		{
-			esc += 100000;		// Habilita o turbo para a subida
-		} 
-		else if (rampa == DESCIDA)
-		{
-			esc = ESC_NEUTRO;	// Habilita o freio para a descida
-		}
-		if (rampa > 0)
-		{
-			if (++tempo_rampa >= TEMPO_RAMPA)
+		#ifdef TRAMENTO_RAMPA
+			// Tratamento da rampa -------------------------------------------------------------------------------------
+			if (rampa == SUBIDA)
 			{
-				rampa = 0;
-				tempo_rampa = 0;
+				esc += 100000;		// Habilita o turbo para a subida
 			}
-		}
+			else if (rampa == DESCIDA)
+			{
+				esc = ESC_NEUTRO;	// Habilita o freio para a descida
+			}
+			if (rampa > 0)
+			{
+				if (++tempo_rampa >= TEMPO_RAMPA)
+				{
+					rampa = 0;
+					tempo_rampa = 0;
+				}
+			}
+		#endif
 
 		// Acionamento dos motores -------------------------------------------------------------------------------------
 		pwms.updateServo(servo);  // Atualiza a posição do servo de acordo com o controle
@@ -122,8 +134,10 @@ int main()
 
         	// Reinicialização das variáveis
         	x = SP_X; y = 0; erro[0] = 0; erro[1] = 0;
-        	ticks_rampa = -10, rampa = 0;
         	reconhecimento.centro_frame_ant = SP_X;
+			#ifdef TRAMENTO_RAMPA
+        		ticks_rampa = -INTERVALO_RAMPA, rampa = 0;
+			#endif
 
         	// Rotina de inicialização
         	leds.setColor(RED);
@@ -135,7 +149,9 @@ int main()
 			usleep(2000000);
 			leds.setColor(GREEN);
 			imagem.open();
-			ticks = 0;
+			#ifdef TRAMENTO_RAMPA
+				ticks = 0;
+			#endif
 			#ifdef DEBUG
 				printf("Robo em funcionamento...\n");
 			#endif
@@ -146,8 +162,16 @@ int main()
         if(buttons.getStatus() == LEFT)
         {
         	v_min = trimpot2.getValue();
-        	usleep(1000);
+        	controle.setVelocMin(v_min);
+        	usleep(10000);
+
         	v_max = trimpot1.getValue();
+        	controle.setVelocMax(v_max);
+        	usleep(10000);
+
+        	kp = trimpot0.getValue();
+        	controle.setKp(kp);
+        	usleep(10000);
         }
 
 
@@ -168,8 +192,11 @@ int main()
 				printf("Numero de linhas OK: %d\n", n_linhas_ok);
 				printf("Servo: %d\n", servo);
 				printf("ESC (%d - %d): %d\n", v_min, v_max, esc);
+				printf("Kp: %d\n", kp);
 
-				printf("Ticks: %d\n", ticks);
+				#ifdef TRATAMENTO_RAMPA
+					printf("Ticks: %d\n", ticks);
+				#endif
 			}
 		#endif
     }
@@ -180,38 +207,39 @@ int main()
 }
 
 
-
-// Thread auxiliar: 100 ms -> 10 Hz ************************************************************************************
-void* tickTimerThread(void *ticks_void_ptr)
-{
-	unsigned int *ticks_ptr = (unsigned int *)ticks_void_ptr;
-
-    int16_t gy;
-    MPU6050 accelgyro;
-    accelgyro.initialize();
-
-	while(1)
+#ifdef TRAMENTO_RAMPA
+	// Thread auxiliar: 100 ms -> 10 Hz ********************************************************************************
+	void* tickTimerThread(void *ticks_void_ptr)
 	{
-		(*ticks_ptr)++;
+		unsigned int *ticks_ptr = (unsigned int *)ticks_void_ptr;
 
-		// Realiza a leitura do eixo Y do giroscópio
-        gy = accelgyro.getRotationY();
+		int16_t gy;
+		MPU6050 accelgyro;
+		accelgyro.initialize();
 
-        // Verifica os eventos de subida ou descida, não permite eventos sucessivos em um intervalo de tempo
-        if(gy < GY_SUBIDA && ((*ticks_ptr - ticks_rampa) >= INTERVALO_RAMPA))	// Identificação da subida
-        {
-        	ticks_rampa = *ticks_ptr;
-        	rampa = SUBIDA;
-    	}
-        else if(gy > GY_DESCIDA && ((*ticks_ptr - ticks_rampa) >= INTERVALO_RAMPA))	// Identificação da descida
-        {
-        	ticks_rampa = *ticks_ptr;
-        	rampa = DESCIDA;
-        }
+		while(1)
+		{
+			(*ticks_ptr)++;
 
-		usleep(100000);
+			// Realiza a leitura do eixo Y do giroscópio
+			gy = accelgyro.getRotationY();
+
+			// Verifica os eventos de subida ou descida, não permite eventos sucessivos em um intervalo de tempo
+			if(gy < GY_SUBIDA && ((*ticks_ptr - ticks_rampa) >= INTERVALO_RAMPA))	// Identificação da subida
+			{
+				ticks_rampa = *ticks_ptr;
+				rampa = SUBIDA;
+			}
+			else if(gy > GY_DESCIDA && ((*ticks_ptr - ticks_rampa) >= INTERVALO_RAMPA))	// Identificação da descida
+			{
+				ticks_rampa = *ticks_ptr;
+				rampa = DESCIDA;
+			}
+
+			usleep(100000);
+		}
+
+		return NULL;
 	}
-
-	return NULL;
-}
+#endif
 
